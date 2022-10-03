6acd46896ca0abae7f331b507c1899b71d23a33e
Muhammad Reziq Dwi Putera <reziqputra@gmail.com>
2022-10-03 22:59:54 +0800

    Create method BubbleSortArray

--- Activity 1/Program.cs
+++ Activity 1/Program.cs
@@ -17,7 +17,7 @@
                 Console.Write("Masukkan banyaknya elemen pada Array: ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
-                if (n <= 20) 
+                if (n <= 20)
                     break;
                 else
                     Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
@@ -48,5 +48,42 @@
             }
             Console.WriteLine("");
         }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // for n - 1 passes
+            {
+                //pada pass i, bandingkan n - 1 elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[j] > a[j + 1]) // jika elmen tidak dalam urutan yang benar
+                    {
+                        // tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // crwaring the object of the BubbleSort class
+            Program mylist = new Program();
+
+            //pemanggilan fungsi untuk menerima elemen array
+            mylist.read();
+
+            //pemanggilan fungsi untuk mengurutkan array 
+            mylist.BubbleSortArray();
+
+            //pemanggilan fungsi untuk menampilkan array yang tersusun
+            mylist.display();
+
+            //exit
+            Console.WriteLine("\n\n Tekan tombol apa saja untuk keluar.");
+            Console.Read();
+        }
     }
+
 }
